470fdc9d466f814e9dd407adff3467b924096a41
Ailton Junior <ailton.junior@starkeparts.com>
2026-09-01 09:07:43 -0300

    limpeza: remoção de arquivos não utilizados e atualização de estilos

--- app/unidades/page.tsx
+++
@@ -1,7 +0,0 @@
-import { StarkePage } from "../page";
-
-export const metadata = { title: "Unidades | Stärke Parts" };
-
-export default function UnidadesPage() {
-  return <StarkePage initialSection="estrutura" />;
-}
